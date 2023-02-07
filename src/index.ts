c4bc27641b2a547e578fa7b39c2345ab22a85802
Christian Bravo <christian.bravo@masglobalconsulting.com>
2023-02-07 17:18:06 -0500

    Added nodemon and support to node and ts-node to work with node modules

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { logger } from "./logger";
+
+const init = () => {
+  logger("Hello world");
+};
+
+init();
---
+++ src/logger.ts
@@ -0,0 +1,3 @@
+export const logger = (value: unknown) => {
+  console.log(value);
+};
